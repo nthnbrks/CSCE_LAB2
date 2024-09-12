617082f23d4ab571007bf59f61e39834148c88be
nthnbrks <nthnbrks@tamu.edu>
2024-09-12 15:31:23 -0500

    implemented required commands

--- shell.cpp
+++ shell.cpp
@@ -12,16 +12,29 @@ int main () {
 
     // TODO: add functionality
     // Create pipe
-
+    int p[2];
+    pipe(p);
     // Create child to run first command
+    pid_t child1 = fork();
     // In child, redirect output to write end of pipe
+    if (child1 == 0){
+        dup2(p[1], STDOUT_FILENO);
     // Close the read end of the pipe on the child side.
+        close(p[0]);
     // In child, execute the command
-
+        execvp(cmd1[0], cmd1);
+    }
     // Create another child to run second command
+    pid_t child2 = fork();
     // In child, redirect input to the read end of the pipe
+    if (child2 == 0){
+        dup2(p[0], STDIN_FILENO);
     // Close the write end of the pipe on the child side.
+        close(p[1]);
     // Execute the second command.
-
+        execvp(cmd2[0], cmd2);
+    }
     // Reset the input and output file descriptors of the parent.
+    close (p[0]);
+    close (p[1]);
 }
